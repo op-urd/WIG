5ef25a63471a8bed22320f1dd37e53468b993a45
nabetama <mao.nabeta@gmail.com>
2017-07-25 23:38:57 +0900

    Add token

---
+++ nabetama/monky/token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// TokenTypes
+const (
+	ILLEGAL = "ILLEGAL" // illegal characters
+	EOF     = "EOF"     // end of the file
+	IDENT   = "IDENT"   // add, foobar, x
+	INT     = "INT"     // 1,2,3...
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Functions
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
