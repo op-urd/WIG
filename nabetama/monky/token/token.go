e7a1b2a33cccdbb07019c5e8c0514c721537acec
nabetama <mao.nabeta@gmail.com>
2017-08-02 12:50:22 +0900

    P.30

--- nabetama/monky/token/token.go
+++ nabetama/monky/token/token.go
@@ -35,14 +35,24 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
-	// Functions
+	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
