aa71723509e88b1438dfe9fdc49e64ed6bab0763
satoshun <shun.sato1@gmail.com>
2017-09-13 23:46:36 +0000

    Eval: impl basic structure, it's starting point

---
+++ stsn/evaluator.go
@@ -0,0 +1,26 @@
+package main
+
+func Eval(node Node) Object {
+	switch node := node.(type) {
+	// Statements
+	case *Program:
+		return evalStatements(node.Statements)
+
+	case *ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *IntegerLiteral:
+		return &Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []Statement) Object {
+	var result Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
