703f619845b4c6ac788af72894280b7831359da9
nabetama <mao.nabeta@gmail.com>
2017-09-19 14:44:21 +0900

    P67 ok

--- nabetama/monky/ast/ast.go
+++ nabetama/monky/ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/op-urd/WIG/nabetama/monky/token"
+import (
+	"bytes"
+
+	"github.com/op-urd/WIG/nabetama/monky/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -20,6 +25,24 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
 type LetStatement struct {
 	Token token.Token // token.LET token
 	Name  *Identifier
@@ -29,13 +52,54 @@ type LetStatement struct {
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
 }
 
+func (i *Identifier) ExpressionNode()      {}
 func (i *Identifier) statementNode()       {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string {
+	return i.Value
+}
 
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
